db1dae65bda9c428a6a559c34f4193a786fccb23
joybappy2 <fan328547@gmail.com>
2025-12-16 14:33:40 +0600

    mongodb connected, api created users post, users get

--- index.js
+++ index.js
@@ -1,11 +1,68 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(cors());
+app.use(express.json());
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.send("E-Tution Server is Running...!");
+});
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const db = client.db("TutionBD_DB");
+    const usersCollection = db.collection("users");
+
+    // users api
+    app.post("/users", async (req, res) => {
+      try {
+        const newUser = req.body;
+        const result = await usersCollection.insertOne(newUser);
+        res.send(result);
+      } catch (err) {
+        res.status(500).send({ error: "Internal Server Error" });
+      }
+    });
+
+    app.get("/users", async (req, res) => {
+      try {
+        const cursor = await usersCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      } catch (err) {
+        res.status(500).send({ error: "Users Not Found" });
+      }
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
